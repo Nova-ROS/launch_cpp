e168301db0932cbb7f96e358788809beb21540d0
hackin zhao <hackin.zhao@qq.com>
2026-03-07 20:56:18 +0800

    Update license headers to Nova ROS, Inc.

--- include/cpp_launch/error_code.hpp
+++ include/cpp_launch/error_code.hpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Example Author
+// Copyright 2026 Nova ROS, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 
 #ifndef CPP_LAUNCH__ERROR_CODE_HPP_
 #define CPP_LAUNCH__ERROR_CODE_HPP_
--- src/actions/declare_launch_argument.cpp
+++ src/actions/declare_launch_argument.cpp
@@ -1,4 +1,4 @@
-// Copyright 2024 Example Author
+// Copyright 2026 Nova ROS, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 
 #include "cpp_launch/actions/declare_launch_argument.hpp"
 #include "cpp_launch/launch_context.hpp"
